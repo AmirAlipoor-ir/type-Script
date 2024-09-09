23c4e70bca0a1f52cf884769f6106257f2fa9076
Amir <amir2005alipoor@gmail.com>
2024-09-09 22:27:05 +0330

    refactore: interfaces in ts

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,51 @@
-// classes
-class Invoice {
-  // readonly client: string;
-  // private details: string;
-  // public amount: number;
+// import { Invoice } from './classes/Invoice.js';
 
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
-  }
+// interfaces
+export interface IsPerson {
+  name: string;
+  age?: number;
+  speak(a: string): void;
+  spend(a: number): number;
 }
 
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+const me: IsPerson = {
+  name: 'shaun',
+  //age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  },
+};
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
+console.log(me);
+me.speak('hello, world');
 
-invoices.forEach((inv) => {
-  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
+const greetPerson = (person: IsPerson): void => {
+  console.log('hello ', person.name);
+}
 
-const form = document.querySelector(".new-item-form") as HTMLFormElement;
+greetPerson(me);
+//greetPerson({name: 'shaun'});
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
 
 // inputs
-const type = document.querySelector("#type") as HTMLInputElement;
-const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
-const details = document.querySelector("#details") as HTMLInputElement;
-const amount = document.querySelector("#amount") as HTMLInputElement;
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-form.addEventListener("submit", (e: Event) => {
+form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
 });
