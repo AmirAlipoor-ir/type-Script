ffead7d625c6aa2fa54305c1eac940ee83b53247
Amir <amir2005alipoor@gmail.com>
2024-09-09 10:04:33 +0330

    fix: fix bug

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,22 @@
 var character = 'mario';
 var age;
 var isLoggedIn;
+// age = 'luigi';
 age = 30;
+// isLoggedIn = 25;
 isLoggedIn = true;
+// arrays
 var ninjas = [];
 ninjas.push('ryu');
 ninjas.push('chun-li');
 console.log(ninjas);
+// union types
 var mixed = [];
 mixed.push('hello');
 mixed.push(false);
 mixed.push(20);
-var uid;
+console.log(mixed);
+// objects
 var ninjaOne;
 ninjaOne = { name: 'yoshi', age: 30 };
 var ninjaTwo;
